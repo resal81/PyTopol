56f517f6a69680ca41997ff70f9ca857e87fa91a
Reza <reza@mac>
2013-09-15 18:33:08 -0400

    system tests cleanup

--- test/systems/cmp_namd_gmx.py
+++ test/systems/cmp_namd_gmx.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
 
+
+# imports
 from __future__ import print_function
 import os,  subprocess, shutil
 import _config as config
 
-def run_command(command, logfile='runlog.log'):
 
+
+
+def run_command(command, logfile='runlog.log'):
+    '''runs a process and reports its output and status
+
+    Gets:
+        command  -> a list of arguments, e.g. ['ls' , '-l']
+        logfile  -> if not None, it is a file to store the output
+
+    Returns:
+        stdout   -> both stdout + stderr
+        status   -> bool, the status of the command (False if the return code is != 0)
+
+    '''
     process = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
     stdout, stderr = process.communicate()
@@ -29,6 +44,12 @@ def run_command(command, logfile='runlog.log'):
 
     return stdout, status
 
+
+
+
+# ===================================================================
+# NAMD
+# ===================================================================
 
 def run_namd(k, logfile):
     psf = config.systems[k]['psf']
@@ -74,10 +95,11 @@ def parse_namd_output(output):
     return result
 
 
+# ===================================================================
+# PSF2TOP
+# ===================================================================
 def run_psf2top(k, logfile):
     psf = config.systems[k]['psf']
-    pdb = config.systems[k]['pdb']
-    #pars = ' '.join(['%s' % x for x in config.systems[k]['pars']])
 
     command = ['%s' % config.paths['psf2top']] + \
               ['-p', psf] + \
@@ -92,6 +114,10 @@ def run_psf2top(k, logfile):
         return stdout, True
 
 
+
+# ===================================================================
+# GROMACS
+# ===================================================================
 def run_gromacs(k, logfile):
     top = 'top.top'
     pdb = config.systems[k]['pdb']
@@ -133,14 +159,16 @@ def parse_gromacs_output():
     for i, line in enumerate(lines):
         line = line.strip()
         if line.startswith('Step') and lines[i+1].strip().startswith('0'):
+
             if lines[i+3].strip().startswith('Energies'):
                 energy_lines = lines[i+4: i+10]
                 if lines[i+11].strip() != '':
                     print('warning: the gromacs energies are in more than 6 lines.')
                 break
+
             elif lines[i+4].strip().startswith('Energies'):
                 energy_lines = lines[i+5: i+11]
-                if lines[i+11].strip() != '':
+                if lines[i+12].strip() != '':
                     print('warning: the gromacs energies are in more than 6 lines.')
                 break
             else:
@@ -210,6 +238,11 @@ def parse_gromacs_output():
     return result
 
 
+
+# ===================================================================
+# MAIN function
+# ===================================================================
+
 def main():
 
     logfile = os.path.abspath('runlog.log')
@@ -217,25 +250,30 @@ def main():
         os.remove(logfile)
 
     maindir = os.getcwd()
-    systems = list(config.systems.keys())
-    systems.sort()
+
+    systems = config.systems
+    # from the _config file, get systems
+    systems_keys = list(systems.keys())
+    systems_keys.sort()
 
     print(' ')
 
-    for k in systems:
 
+    for k in systems_keys:
 
+        # if the output directory for the test `k` still exists, remove it
         if os.path.exists(k):
             shutil.rmtree(k)
 
-        if config.systems[k]['skip'] == True:
+        # should we skip this test ?
+        if k in config.skip_systems:
             continue
 
         # get the absolute paths
-        config.systems[k]['psf'] = os.path.abspath(config.systems[k]['psf'])
-        config.systems[k]['pdb'] = os.path.abspath(config.systems[k]['pdb'])
-        for i, m in enumerate(config.systems[k]['pars']):
-            config.systems[k]['pars'][i] = os.path.abspath(m)
+        systems[k]['psf'] = os.path.abspath(systems[k]['psf'])
+        systems[k]['pdb'] = os.path.abspath(systems[k]['pdb'])
+        for i, m in enumerate(systems[k]['pars']):
+            systems[k]['pars'][i] = os.path.abspath(m)
 
 
         print(('running system: %s' % k))
@@ -261,7 +299,7 @@ def main():
             return
         else:
             result = parse_gromacs_output()
-            config.systems[k]['gromacs_result'] = result
+            systems[k]['gromacs_result'] = result
 
 
         # go one level up
@@ -280,7 +318,7 @@ def main():
             with open('namd.out', 'w') as f:
                 f.writelines([output])
             result = parse_namd_output(output)
-            config.systems[k]['namd_result'] = result
+            systems[k]['namd_result'] = result
 
 
         # go one level up
@@ -289,61 +327,93 @@ def main():
         # go back to the main directory
         os.chdir(maindir)
 
+    summarize_test_outputs(systems)
+
+
+
+
+def summarize_test_outputs(systems):
+    '''
+    Gets:
+        config.systems with the energies e.g.:
+            {
+                '101': {
+                    'psf': '...',
+                    'gromacs_result' : {},
+                    'namd_result' : {},
+                }
+            }
+    '''
 
     print(' ')
-    for k in systems:
-        if config.systems[k]['skip'] == True:
+    system_keys = list(systems.keys())
+    system_keys.sort()
+
+
+    # detail summary ----------------------------
+    desc = 'Table 1. Summary of the NAMD and GROMACS energies.\n'
+    print(desc)
+
+    for k in system_keys:
+        if k in config.skip_systems:
             continue
 
-        print(('%s - %s' % (k, config.systems[k]['info'])))
-        print(('{:10s}   {:>10s}   {:>10s}   {:>9s}  {:>9s}'.format(
-            k, 'NAMD', 'GROMACS', 'Diff', '%Diff')) )
+        print('-' * 59)
+        heading = '%s - %s \n' % (k, systems[k]['info'])
+        heading+= '{:10s}   {:>10s}   {:>10s}   {:>9s}  {:>9s}'.format(
+            '', 'NAMD', 'GROMACS', 'GMX-NAMD', '% |diff|')
+        print(heading)
+
         for m in ('bond', 'angle', 'dihedral', 'improper', 'coul', 'vdw'):
-            namd = config.systems[k]['namd_result'][m]
-            gromacs = config.systems[k]['gromacs_result'][m]
+            namd = systems[k]['namd_result'][m]
+            gromacs = systems[k]['gromacs_result'][m]
 
             diff =  gromacs - namd
 
-            if (abs(gromacs - namd) < 0.01):
-                diffp = '0.0'
-            elif abs(namd) <= 0.001:
-                diffp = 'NA'
+            if namd == 0:
+                diffp = '0.00'
             else:
-                diffp = '%5.1f' % (((gromacs-namd)/namd) * 100.0)
-            s = '%10s   %10.1f   %10.1f   %9.1f  %9s' % (m, namd, gromacs, diff, diffp)
-            print(s)
+                diffp = abs(  ((gromacs-namd)/namd) * 100.0  )
+                diffp =  '%5.2f' % (diffp)
+
+            result = '%10s   %10.2f   %10.2f   %9.2f  %9s' % (m, namd, gromacs, diff, diffp)
+            print(result)
 
         print(' ')
 
-    for k in systems:
-        if config.systems[k]['skip'] == True:
+    print('-' * 59)
+
+    # short summary -----------------------------
+    print('\n\n')
+    desc = 'Table 2. Summary of the difference between GROMACS and NAMD energies.'
+    print(desc)
+    print('-' * 110)
+    print('{:12s}  {:6s}  {:4s} {:^13s} {:^13s} {:^13s} {:^13s} {:^13s} {:^13s}'.format(
+           ' ','natoms', 'ff', 'bond', 'angle', 'dihedral', 'improper', 'coul', 'vdw'))
+
+    print('-' * 110)
+
+    for k in system_keys:
+        if k in config.skip_systems:
             continue
 
-        print(('%s - %s' % (k, config.systems[k]['info'])))
-
-    print(' ')
-    print('%6s  %13s %13s %13s %13s %13s %13s ' % ('','bond', 'angle', 'dihedral', 'improper', 'coul', 'vdw'))
-
-    for k in systems:
-        if config.systems[k]['skip'] == True:
-            continue
-        s = '%6s  ' % k
+        sys = systems[k]
+        s = '%12s  %6d  %4s  ' % (sys['name'], sys['natoms'], sys['ff'] )
         for m in ('bond', 'angle', 'dihedral', 'improper', 'coul', 'vdw'):
-            namd = config.systems[k]['namd_result'][m]
-            gromacs = config.systems[k]['gromacs_result'][m]
+            namd = systems[k]['namd_result'][m]
+            gromacs = systems[k]['gromacs_result'][m]
 
             diff =  gromacs - namd
 
-            if (abs(gromacs - namd) < 0.01):
-                diffp = '0.000'
-            elif abs(namd) <= 0.001:
-                diffp = 'NA'
+            if namd == 0:
+                diffp = '0.0'
             else:
-                diffp = '%5.3f' % (((gromacs-namd)/namd) * 100.0)
-            s += '%4.1f (%6s)  ' % (diff, diffp)
+                diffp = abs(  ((gromacs-namd)/namd) * 100.0  )
+                diffp =  '%4.1f' % (diffp)
+            s += '%5.2f (%4s)  ' % (diff, diffp)
 
         print(s)
-
+    print('-' * 110)
 
 if __name__ == '__main__':
     main()
